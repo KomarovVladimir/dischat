2532a51900d6b0919e457183a0156736afcba6d8
KomarovVladimir <komarov2142@gmail.com>
2024-03-02 07:02:04 +0300

    Add an input logic test

--- src/features/chat/__tests__/Chat.test.tsx
+++ src/features/chat/__tests__/Chat.test.tsx
@@ -1,8 +1,9 @@
 import "@testing-library/jest-dom";
+import { screen } from "@testing-library/dom";
 import { Routes, Route, MemoryRouter } from "react-router";
 
 import { setupStore } from "app/store";
-import { renderWithProviders } from "common/utils/test-utils";
+import { fireEvent, renderWithProviders } from "common/utils/test-utils";
 import { roomAdded } from "features/rooms/slice/roomsSlice";
 import { routes } from "routing/routes";
 
@@ -11,7 +12,7 @@ import { messageAdded } from "../slice/messagesSlice";
 
 window.HTMLElement.prototype.scrollIntoView = function () {};
 
-test("Renders Chat component", () => {
+test("Renders a chat with three messages", () => {
     const store = setupStore();
     const {
         payload: { id: roomId }
@@ -33,4 +34,34 @@ test("Renders Chat component", () => {
     expect(getByText("Message 1")).toBeInTheDocument();
     expect(getByText("Message 2")).toBeInTheDocument();
     expect(getByText("Message 3")).toBeInTheDocument();
+});
+
+test("Adds a message to a chat", () => {
+    const store = setupStore();
+    const {
+        payload: { id: roomId }
+    } = store.dispatch(roomAdded("Room 1"));
+
+    const { getByText } = renderWithProviders(
+        <MemoryRouter initialEntries={[`/rooms/${roomId}`]}>
+            <Routes>
+                <Route path={routes.room} element={<Chat />} />
+            </Routes>
+        </MemoryRouter>,
+        { store }
+    );
+
+    const inputElement = screen.getByPlaceholderText("Write a message...");
+
+    const messageText = "A message";
+
+    fireEvent.change(inputElement, { target: { value: messageText } });
+
+    fireEvent.keyUp(inputElement, {
+        key: "Enter",
+        code: "Enter",
+        charCode: 13
+    });
+
+    expect(getByText(messageText)).toBeInTheDocument();
 });
