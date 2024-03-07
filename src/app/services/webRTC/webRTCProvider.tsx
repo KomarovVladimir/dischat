d2620967249f5c2af1c3dcc94b349330e0ccf470
KomarovVladimir <komarov2142@gmail.com>
2024-03-07 14:11:36 +0300

    Update webRTCProvider.tsx

--- src/app/services/webRTC/webRTCProvider.tsx
+++ src/app/services/webRTC/webRTCProvider.tsx
@@ -1,19 +1,9 @@
-import { PropsWithChildren, createContext, useContext } from "react";
+import { PropsWithChildren, createContext } from "react";
 import { WebRTCService } from "services/WebRTCService ";
 
 const webRTCService = new WebRTCService();
 
 export const WebRTCContext = createContext(webRTCService);
-
-export const useWebRTC = () => {
-    const context = useContext(WebRTCContext);
-
-    if (!context) {
-        throw new Error("useWebRTC must be used within a WebRTCProvider");
-    }
-
-    return context;
-};
 
 export const WebRTCProvider = ({ children }: PropsWithChildren) => (
     <WebRTCContext.Provider value={webRTCService}>
