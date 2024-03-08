f217cdf4d0305b01077dab610068e897793442c7
KomarovVladimir <komarov2142@gmail.com>
2024-03-08 05:14:08 +0300

    Update roomsApi.ts

--- src/features/rooms/api/roomsApi.ts
+++ src/features/rooms/api/roomsApi.ts
@@ -1,22 +1,24 @@
+// import { EntityId } from "@reduxjs/toolkit";
 import { api } from "app/services/api";
 
-import { type AddRoomRequest } from "../types";
-import { type RoomEntity } from "../slice/roomsSlice";
+// import { type RoomEntity } from "../slice/roomsSlice";
+
+// type AddRoomRequest = { name: string } | { roomId: EntityId };
 
 //TODO: Replace body with params
 //TODO: Update the return value
 export const roomsApi = api.injectEndpoints({
     endpoints: (builder) => ({
-        addRoom: builder.mutation<RoomEntity, AddRoomRequest>({
-            query: (body) => ({
-                url: "/rooms",
-                method: "POST",
-                body
-            }),
-            invalidatesTags: ["Rooms"],
-            transformResponse: ({ data }) => data
-        })
+        // addRoom: builder.mutation<RoomEntity, AddRoomRequest>({
+        //     query: (body) => ({
+        //         url: "/rooms",
+        //         method: "POST",
+        //         body
+        //     }),
+        //     invalidatesTags: ["Rooms"],
+        //     transformResponse: ({ data }) => data
+        // })
     })
 });
 
-export const { useAddRoomMutation } = roomsApi;
+// export const { useAddRoomMutation } = roomsApi;
